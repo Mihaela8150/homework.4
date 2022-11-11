d2f204ae8042c6f3e589d20977f13b9f342ab7a8
Mihaela8150 <114097144+Mihaela8150@users.noreply.github.com>
2022-11-11 20:19:46 +0200

    Add files via upload

---
+++ homework4/javas.js
@@ -0,0 +1,12 @@
+let num = 0;
+
+function addOneHandle() {
+    num++;
+    document.getElementById('display').innerHTML =num;
+}
+function minusOneHandle() {
+    num--;
+    document.getElementById('display').innerHTML =num;
+}
+document.getElementById('plus').addEventListener("click",addOneHandle);
+document.getElementById('minus').addEventListener("click",minusOneHandle);
